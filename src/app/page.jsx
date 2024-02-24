932b1c740486568d97c383279f9ecaa3d8bf7211
YenLun <yenlunliu@gmail.com>
2024-02-24 04:01:49 -0800

    basic header

---
+++ src/app/page.jsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+
+export default function Home() {
+    return (
+        <div>hi</div>
+        // <ul className="menu bg-base-200 w-56 rounded-box">
+        //     <li className="menu-title">Title</li>
+        //     <li>
+        //         <a>Item 1</a>
+        //     </li>
+        //     <li>
+        //         <a>Item 2</a>
+        //     </li>
+        //     <li>
+        //         <a>Item 3</a>
+        //     </li>
+        // </ul>
+    );
+}
