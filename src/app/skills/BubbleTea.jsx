21cea0065059976f96a31c839fe50687fedbf748
YenLun <yenlunliu@gmail.com>
2024-02-27 14:18:26 -0800

    bubble tea css

---
+++ src/app/skills/BubbleTea.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./bubbletea.css";
+
+function BubbleTea() {
+    return (
+        <div className="bubbleTeaGrid">
+            <div className="cup"></div>
+            <div className="cupShadow"></div>
+            <div className="lid"></div>
+            <div className="straw"></div>
+            <div className="pearl1">React</div>
+            <div className="pearl2">JavaScript</div>
+        </div>
+    );
+}
+
+export default BubbleTea;
--- src/app/skills/page.jsx
+++ src/app/skills/page.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Title from "../common/Title";
+import BubbleTea from "./BubbleTea";
 
 function Skills() {
-    return <div>This is skills route.</div>;
+    return (
+        <>
+            <div className="flex flex-col w-1/2">
+                <Title text="Skill" />
+                <BubbleTea />
+            </div>
+        </>
+    );
 }
 
 export default Skills;
