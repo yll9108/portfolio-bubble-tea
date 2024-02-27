f68332c7108ad7c9d032903e6dcd81531a5cf71a
YenLun <yenlunliu@gmail.com>
2024-02-26 20:49:19 -0800

    bubble tea shop css without flags

---
+++ src/app/components/home/BubbleTeaShop.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./bubbleteashop.css";
+
+function BubbleTeaShop() {
+    return (
+        <>
+            <div className="storeGrid">
+                <div className="stick1"></div>
+                <div className="stick2"></div>
+                <div className="stick3"></div>
+                <div className="stick4"></div>
+                <div className="signboard">BUBBLE TEA</div>
+                <div className="plate1"></div>
+                <div className="plate2"></div>
+                <div className="plate3"></div>
+                <div className="plate4"></div>
+                <div className="screw1"></div>
+                <div className="screw2"></div>
+                <div className="screw3"></div>
+                <div className="screw4"></div>
+                <div className="screw5"></div>
+                <div className="screw6"></div>
+                <div className="screw7"></div>
+                <div className="screw8"></div>
+            </div>
+        </>
+    );
+}
+
+export default BubbleTeaShop;
