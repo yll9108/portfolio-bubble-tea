d9b3075617f3cbaba7b5a1123608e09de1d85815
YenLun <yenlunliu@gmail.com>
2024-02-29 12:15:06 -0800

    skill section css

--- src/app/skills/BubbleTea.jsx
+++ src/app/skills/BubbleTea.jsx
@@ -11,6 +11,7 @@ function BubbleTea() {
             <div className="wave"></div>
             <div className="logo text-2xl">
                 Chill on <span>BOBA</span>
+                Coding with <span>YEN</span>
             </div>
             {/* {bubbleSkills.map((skill, index) => (
                 <div className={`pearl pearl${index + 1}`} key={index}>
