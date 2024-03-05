24d8191eba6eebc9449e82b6fd93f056d45ea780
YenLun <yenlunliu@gmail.com>
2024-03-05 10:23:06 -0800

    skill as icons

--- src/app/components/skills/SkillIcon.jsx
+++ src/app/components/skills/SkillIcon.jsx
@@ -1,51 +1,51 @@
-import React from "react";
-import "./skillIcon.css";
-import { bubbleSkills } from "./bubbleSkills";
-
-function SkillIcon() {
-    return (
-        <>
-            <div>
-                <ul className="skills">
-                    <li className="skill-icon">
-                        {bubbleSkills.map((skill, index) => (
-                            <img
-                                key={skill.alt}
-                                src={skill.src}
-                                alt={skill.alt}
-                            />
-                        ))}
-                    </li>
-                </ul>
-            </div>
-        </>
-    );
-}
-
-export default SkillIcon;
+// import React from "react";
+// import "./skillIcon.css";
+// import { bubbleSkills } from "./bubbleSkills";
 
 // function SkillIcon() {
 //     return (
 //         <>
-//             <div className="skills">
-//                 {/* <div className="skill-icon">
-//                     <img src="bootstrap-fill-svgrepo-com.svg" alt="BS" />
-//                     <img src="bootstrap-fill-svgrepo-com.svg" alt="BS" />
-//                 </div> */}
-//                 {/* {bubbleSkills.map((skill, index) => (
-//                     <li className={`pearl pearl${index + 1}`} key={index}>
-//                         <img key={skill.alt} src={skill.src} alt={skill.alt} />
+//             <div>
+//                 <ul className="skills">
+//                     <li className="skill-icon">
+//                         {bubbleSkills.map((skill, index) => (
+//                             <img
+//                                 key={skill.alt}
+//                                 src={skill.src}
+//                                 alt={skill.alt}
+//                             />
+//                         ))}
 //                     </li>
-//                 ))} */}
+//                 </ul>
 //             </div>
 //         </>
 //     );
 // }
 
-// {
-/* {bubbleSkills.map((skill, index) => (
-                <div className={`pearl pearl${index + 1}`} key={index}>
-                    <img key={skill.alt} src={skill.src} alt={skill.alt} />
-                </div>
-            ))} */
-// }
+// export default SkillIcon;
+
+// // function SkillIcon() {
+// //     return (
+// //         <>
+// //             <div className="skills">
+// //                 {/* <div className="skill-icon">
+// //                     <img src="bootstrap-fill-svgrepo-com.svg" alt="BS" />
+// //                     <img src="bootstrap-fill-svgrepo-com.svg" alt="BS" />
+// //                 </div> */}
+// //                 {/* {bubbleSkills.map((skill, index) => (
+// //                     <li className={`pearl pearl${index + 1}`} key={index}>
+// //                         <img key={skill.alt} src={skill.src} alt={skill.alt} />
+// //                     </li>
+// //                 ))} */}
+// //             </div>
+// //         </>
+// //     );
+// // }
+
+// // {
+// /* {bubbleSkills.map((skill, index) => (
+//                 <div className={`pearl pearl${index + 1}`} key={index}>
+//                     <img key={skill.alt} src={skill.src} alt={skill.alt} />
+//                 </div>
+//             ))} */
+// // }
--- src/app/skills/page.jsx
+++ src/app/skills/page.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import Title from "../common/Title";
 import BubbleTea from "../components/skills/BubbleTea";
-import SkillIcon from "../components/skills/SkillIcon";
+// import SkillIcon from "../components/skills/SkillIcon";
 
 function Skills() {
     return (
         <>
             <div className="flex m-auto flex-col">
                 <Title text="Skill" />
-                <div className="flex gap-4">
-                    <SkillIcon />
-                    <BubbleTea />
-                </div>
+                {/* <div className="flex gap-4"> */}
+                {/* <SkillIcon /> */}
+                <BubbleTea />
             </div>
+            {/* </div> */}
         </>
     );
 }
