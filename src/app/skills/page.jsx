d4ce9918724a34eaff1c8e72b7651f84f4d28928
YenLun <yenlunliu@gmail.com>
2024-03-06 19:26:57 -0800

    skill mobile done

--- src/app/skills/page.jsx
+++ src/app/skills/page.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Title from "../common/Title";
 import BubbleTea from "../components/skills/BubbleTea";
+import SkillList from "../components/skills/SkillList";
 // import SkillIcon from "../components/skills/SkillIcon";
 
 function Skills() {
@@ -8,9 +9,16 @@ function Skills() {
         <>
             <div className="flex flex-col justify-center items-center sm:m-auto">
                 <Title text="Skill" />
+                {/* <div className="flex flex-row">
+                    <div>123</div>
+                    <div>123</div>
+                </div> */}
                 {/* <div className="flex gap-4"> */}
                 {/* <SkillIcon /> */}
-                <BubbleTea />
+                <div className="sm:flex">
+                    <SkillList />
+                    <BubbleTea />
+                </div>
             </div>
             {/* </div> */}
         </>
