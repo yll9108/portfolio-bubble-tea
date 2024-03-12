2d721c1d471d75438ab0cd546291b625a33b57c7
YenLun <yenlunliu@gmail.com>
2024-03-12 13:20:35 -0700

    fix key error

--- src/app/components/skills/SkillList.jsx
+++ src/app/components/skills/SkillList.jsx
@@ -7,7 +7,7 @@ function SkillList() {
         <>
             <div className="skillListContainer flex flex-wrap  flex-col sm:gap-1 m-auto">
                 {bubbleSkills.map((skill, index) => (
-                    <div className="flex gap-2">
+                    <div key={index} className="flex gap-2">
                         <img className="h-10" src={skill.src} />
                         <span className="flex items-center">{skill.alt}</span>
                     </div>
